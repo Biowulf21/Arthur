9f08bf73177f455730ab1450ec98f37ad001d9ab
James Jilhaney <notail2111@gmail.com>
2022-01-27 16:09:25 +0800

    added working progress bar and textedit now works

--- arthur.py
+++ arthur.py
@@ -18,6 +18,40 @@ class UI(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # declaring UI elements
+
+        # declaring buttons
+        self.ui.sendMailBtn.clicked.connect(self.sendMail)
+
+        self.ui.progressBar.setValue(0)
+
+    def sendMail(self):
+        receipientList = []
+        # gets the emails from the receipient text edit and turns it into individual strings
+        emails = self.ui.receipientTextEdit.toPlainText()
+        receipientList = emails.split()
+
+        # list of emails that are successfuly sent
+        sentList = []
+        # list of emails that were unsuccessfuly sent
+        unsentList = []
+
+        # setting the progress bar
+        maxValue = len(receipientList)
+        self.ui.progressBar.setMaximum(maxValue)
+        print(f'number of recepients are: {maxValue}')
+
+        # set up minimum value for progressbar
+        progressValue = 1
+
+        for recepient in receipientList:
+            # update progress bar based on the how many receipients have been sent and email already
+            sendMail.bulkSendMail(recepient)
+            self.ui.progressBar.setValue(progressValue)
+            progressValue += 1
+            # add email to sent list
+            sentList.append(recepient)
+
 
 if __name__ == "__main__":
     app = QApplication([])
--- sendMail.py
+++ sendMail.py
@@ -1,1 +1,8 @@
 import smtplib
+from time import sleep
+
+
+def bulkSendMail(receipient):
+    sleep(3)
+    print(f'sent to {receipient}')
+    print('Done sending')
